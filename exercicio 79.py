b948597fdf505bc8e4d878e3ba7ecfa82366f543
LTbirdyy <gabriel.fernando@sempreceub.com>
2025-03-26 20:47:12 -0300

    Exercicio 79

--- exercicio 79.py
+++ exercicio 79.py
@@ -1,1 +1,13 @@
-print('Teste novo')
+lista = []
+cont = 0
+while True:
+    cont += 1
+    numeros = int(input('Fale alguns valores:'))
+    if numeros not in lista:
+        lista.append(numeros)
+    if cont >= 3:
+        continuar = input('Deseja continuar[Y/N]').strip().upper()[0]
+        cont -= 3
+        if continuar == 'N':
+            break
+print(sorted(lista))
